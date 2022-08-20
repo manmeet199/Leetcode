9e1908e8d08558556b8e788be896c3e07839a549
Manmeet Singh Chhabra <manmeett29@gmail.com>
2022-08-21 00:46:09 +0530

    Time: 216 ms (19.57%), Space: 67.8 MB (54.19%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        //kadane
+        int n=nums.size();
+        int sum=0;
+        int ans=INT_MIN;
+        for(int i=0; i<n; i++)
+        {
+         sum+=nums[i];
+        ans=max(ans,sum);
+          if(sum<0)
+          {
+              sum=0;
+          }
+              
+            }
+        return ans;
+    }
+};
