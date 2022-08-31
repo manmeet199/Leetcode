71c4e9687e2ffa9dfdcc0a14721673aa8cb16a1d
Manmeet Singh Chhabra <manmeett29@gmail.com>
2022-09-01 02:32:33 +0530

    Time: 12 ms (67.43%), Space: 11.2 MB (60.62%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,86 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+//         Node* iter=head;
+//         Node* front=head;
+//         while(iter!=NULL)
+//         {
+//             front=iter->next;
+//             Node* copy=new Node(iter->val);
+//             iter->next=copy;
+//             copy->next=front;
+//             iter=front;
+//         }
+        
+        
+//         iter=head;
+//         while(iter!=NULL)
+//         {
+//             if(iter->random!=NULL)
+//             {
+
+//             iter->next->random=iter->random->next;
+//             }
+            
+//         iter=iter->next->next;    
+//         }
+        
+        
+//         iter=head;
+//         Node *pseudohead=new Node(0);
+//         Node* copy=pseudohead;
+        
+//         while(iter!=NULL)
+//         {
+//             front=iter->next->next;
+           
+            
+//             copy->next=iter->next;
+//             iter->next=front;
+            
+//             iter=iter->next;
+//             copy=copy->next;
+            
+            
+//         }
+        
+        
+//         return pseudohead->next;
+        
+        
+        unordered_map<Node*, Node*> m;
+        Node* cur=head;
+        while(cur)
+        {
+            m[cur]=new Node(cur->val);
+            cur=cur->next;
+            
+        }
+        cur=head;
+        while(cur)
+        {
+            Node* tmp=m[cur];
+            tmp->next=m[cur->next];
+            tmp->random=m[cur->random];
+            cur=cur->next;
+            
+        }
+        return m[head];
+    }
+};
