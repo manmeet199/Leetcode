b84d5ab53d82ec150a2b206f6023f4ecfd8e4c84
Manmeet Singh Chhabra <manmeett29@gmail.com>
2022-09-06 03:01:33 +0530

    Time: 4 ms (34.13%), Space: 5.9 MB (81.46%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        //Brute dosent work for -ve power
         double ans=1.0;
         long long nn=n;
-        if(nn<0) nn=-1*nn;
+        if(nn<0)
+        {
+            nn=-1*nn;
+            
+        }
         while(nn)
         {
-            if(nn%2)
+            if(nn%2==0)
+            {
+                
+                x=x*x;
+                nn=nn/2;
+                
+            }
+            else
             {
                 ans=ans*x;
                 nn=nn-1;
             }
-            else
-            {
-                x=x*x;
-                nn=nn/2;
-            }
         }
         if(n<0)
-            ans=(double)(1.0)/(double)(ans);
-            return ans;
+            
+        {
+            ans=(double)1.0/(double)ans;
+        }
+        return ans;
     }
 };
