82b8dc03e59bbf0409d08b2f5edb0e0fbae71586
Manmeet Singh Chhabra <manmeett29@gmail.com>
2022-09-05 16:04:46 +0530

    Time: 11 ms (51.70%), Space: 10.7 MB (73.23%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-     void comb(int ind,int target, vector<int>& candidates, vector<vector<int>> &ans, vector<int> &ds)
-    {
-      if(target==0)
-      {
-          ans.push_back(ds);
-      }
-         int i;
-         for(int i=ind; i<candidates.size(); i++)
-         {
-             if(i>ind and candidates[i]==candidates[i-1])
-                 continue;
-             if(candidates[i]>target)
-                 break;
-             ds.push_back(candidates[i]);
-             comb(i+1,target-candidates[i],candidates,ans,ds);
-             ds.pop_back();
-             
-         }
-
-      
+    void solve(vector<int> &nums, int i, int cs, int target, vector<int> &temp, vector<vector<int>> &res){
         
+        if(cs>target)
+        {
+            return;
+        }
+        if(cs==target)
+        {
+            res.push_back(temp);
+            return;
+        }
+        
+        for(int j=i;j<nums.size();j++)
+        {
+            if(i==j or nums[j]!=nums[j-1])
+            {
+                temp.push_back(nums[j]);
+                cs+=nums[j];
+                solve(nums, j+1, cs, target, temp, res);
+                cs-=nums[j];
+                temp.pop_back();
+            }
+        }
     }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-       sort(candidates.begin(),candidates.end());//imp
-           vector<vector<int>> ans;
-        vector<int> ds;
-        comb(0,target,candidates,ans,ds);
-        return ans;
+    
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        vector<int> temp;
+        solve(nums, 0, 0, target, temp, res);
+        return res;
     }
 };
