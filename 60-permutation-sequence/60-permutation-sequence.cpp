70543d98f5caf51e9a7865040376e4a4812abf66
Manmeet Singh Chhabra <manmeett29@gmail.com>
2022-09-06 10:19:37 +0530

    Time: 1644 ms (5.01%), Space: 279.6 MB (17.53%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,50 +1,18 @@
 class Solution {
 public:
-    //   void solve(string & s, int index, vector < string > & res) {
-    //   if (index == s.size()) {
-    //     res.push_back(s);
-    //     return;
-    //   }
-    //   for (int i = index; i < s.size(); i++) {
-    //     swap(s[i], s[index]);
-    //     solve(s, index + 1, res);
-    //     swap(s[i], s[index]);
-    //   }
-    // }
     string getPermutation(int n, int k) {
-        
-      //  O(N!*N)+(N!logN!)
-//      string s;
-//         vector<string> res;
-//         for(int i=1; i<=n; i++)
-//         {
-//             s.push_back(i+'0');
-            
-//         }
-//         solve(s,0,res);
-//         sort(res.begin(),res.end());
-        
-//         auto it=res.begin()+k-1;// as res is  0 indexed
-//         return *it;
-               int fact = 1;
-      vector < int > numbers;
-      for (int i = 1; i < n; i++) {
-        fact = fact * i;
-        numbers.push_back(i);
-      }
-      numbers.push_back(n);
-      string ans = "";
-      k = k - 1;
-      while (true) {
-        ans = ans + to_string(numbers[k / fact]);
-        numbers.erase(numbers.begin() + k / fact);
-        if (numbers.size() == 0) {
-          break;
-        }
+        string s="";
+        for(int i=1; i<=n; i++)
+        {
 
-        k = k % fact;
-        fact = fact / numbers.size();
-      }
-      return ans;
+        s+=to_string(i);
+        }
+        vector<string> seq;
+               seq.push_back(s);
+        while(next_permutation(s.begin(),s.end()))
+        {
+            seq.push_back(s);
+        }
+        return seq[k-1];
     }
 };
