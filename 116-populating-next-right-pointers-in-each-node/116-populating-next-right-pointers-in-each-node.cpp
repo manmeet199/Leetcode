29ebbdd198822eb9e9b01e835cc7a8c40d4c9bf5
Manmeet Singh Chhabra <manmeett29@gmail.com>
2022-09-14 04:03:22 +0530

    Time: 41 ms (25.31%), Space: 17 MB (60.96%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -19,34 +19,35 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(!root)
-            return root;
-        queue<Node* > q;
+    if(!root)
+        return root;
+        queue<Node*> q;
         q.push(root);
         while(!q.empty())
         {
+            
             int n=q.size();
             for(int i=0; i<n; i++)
             {
             auto it=q.front();
             q.pop();
-                if(i!=n-1){// if i==n-1 then it->next==NULL
+                
+                if(i!=n-1)
+                {
                     it->next=q.front();
                 }
-                 if(it->left)
+                
+                      if(it->left)
             {
                 q.push(it->left);
             }
              if(it->right)
             {
                 q.push(it->right);
-            }
-           
-           
             
-           
+            }
         }
         }
-        return root;
+            return root;
     }
 };
