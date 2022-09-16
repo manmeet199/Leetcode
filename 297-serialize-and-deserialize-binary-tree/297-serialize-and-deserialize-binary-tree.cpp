57d5f2bfc340f40bb84cf7a33fe05b9be600a4f1
Manmeet Singh Chhabra <manmeett29@gmail.com>
2022-09-16 19:39:37 +0530

    Time: 130 ms (20.12%), Space: 42.4 MB (20.90%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
@@ -11,41 +11,47 @@ class Codec {
 public:
 
     // Encodes a tree to a single string.
- 
- string serialize(TreeNode* root) {
-    if(!root) {
-     return "NULL,";
+    string serialize(TreeNode* root) {
+        if(!root)
+            return "NULL,";
+        return to_string(root->val)+","+serialize(root->left)+serialize(root->right);
+        
+        
     }
-    return to_string(root->val)+","+serialize(root->left)+serialize(root->right);
-}
 
-// Decodes your encoded data to tree.
-TreeNode* deserialize(string data) {
-    queue<string> q;
-    string s;
-    for(int i=0;i<data.size();i++)
-    {
-        if(data[i]==',')
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+     queue<string> q;
+        string s;
+        for(int i=0; i<data.size(); i++)
+        {
+            if(data[i]==',')
+            {
+                q.push(s);
+                s="";
+                continue;
+            }
+            s+=data[i];
+            
+        }
+        if(s.size()!=0)// for last string
         {
             q.push(s);
-            s="";
-            continue;
         }
-        s+=data[i];
+        return helper(q);
     }
-    if(s.size()!=0)q.push(s);
-    return deserialize_helper(q);
-}
-
-TreeNode* deserialize_helper(queue<string> &q) {
-    string s=q.front();
-    q.pop();
-    if(s=="NULL")return NULL;
-    TreeNode* root=new TreeNode(stoi(s));
-    root->left=deserialize_helper(q);
-    root->right=deserialize_helper(q);
-    return root;
-}
+    
+    TreeNode* helper(queue<string>& q)
+    {
+        string s=q.front();
+        q.pop();
+        if(s=="NULL")
+            return NULL;
+        TreeNode* root=new TreeNode(stoi(s));
+        root->left=helper(q);
+        root->right=helper(q);
+        return root;
+    }
 };
 
 // Your Codec object will be instantiated and called as such:
