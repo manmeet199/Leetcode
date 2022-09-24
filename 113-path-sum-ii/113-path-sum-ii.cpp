b0b76ed03e49da54928530ec1895453141f4cc1d
Manmeet Singh Chhabra <manmeett29@gmail.com>
2022-09-24 09:52:00 +0530

    Time: 12 ms (81.57%), Space: 19.8 MB (81.15%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -11,44 +11,30 @@
  */
 class Solution {
 public:
-    // void getPaths(TreeNode* root, int sum, vector<int> &path , vector<vector<int>> &paths)
-    // {
-    // //     if(!root)
-    // //         return;
-    // //     //else
-    // //     path.push_back(root->val);
-    //     if(!root->left and !root->right and root->val==sum)// for leaf node
-    //     {
-    //         paths.push_back(path);
-    //         getPaths(root->left,sum-root->val,path,paths);
-    //         getPaths(root->right,sum-root->val,path,paths);
-    //         paths.pop_back();
-    //     }
-    // }
-    // vector<vector<int>> pathSum(TreeNode* root, int sum) {
-    //     vector<vector<int>> paths;
-    //     vector<int> path;
-    //     getPaths(root,sum,path,paths);
-    //     return paths;
-    //}
- vector<vector<int>> pathSum(TreeNode* root, int sum) {
-        vector<vector<int> > paths;
-        vector<int> path;
-        findPaths(root, sum, path, paths,0);
-        return paths;  
-    }
-
-    void findPaths(TreeNode* node, int sum, vector<int>& path, vector<vector<int> >& paths, int curSum) {
-        if (!node) return;
-        
-        curSum+=node->val;
-        path.push_back(node -> val);
+    void helper(TreeNode* root, vector<vector<int>> &ans, vector<int> &path, int targetSum, int curSum)
+    {
+        if(!root)
+            return;
+        curSum+=root->val;
+        path.push_back(root->val);
+        if(!root->left and !root->right and curSum==targetSum)
+        {
+            ans.push_back(path);
+        }
+        helper(root->left,ans,path,targetSum,curSum);
+        helper(root->right,ans,path,targetSum,curSum);
         
         
-        if (!(node -> left) && !(node -> right) && sum == curSum)//leaf
-            paths.push_back(path);
-        findPaths(node -> left,sum, path, paths,curSum);
-        findPaths(node -> right, sum, path, paths,curSum);
         path.pop_back();
+        
+    }
+    
+    
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        vector<int> path;
+        helper(root,ans,path,targetSum,0);
+        return ans;
+        
     }
 };
