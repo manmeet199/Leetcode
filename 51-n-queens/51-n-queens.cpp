cce600187a2826dece41dac2983d1efacf2fcbb8
Manmeet Singh Chhabra <manmeett29@gmail.com>
2022-10-03 01:34:19 +0530

    Time: 17 ms (38.08%), Space: 7.3 MB (66.62%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,48 +1,24 @@
 class Solution {
 public:
-    
     vector<vector<string>> res;
-    
-    bool is_Valid(vector<string>& board, int row, int col)
-    {
-       
-        for(int i=row; i>=0; i--)
-        {
-            if(board[i][col]=='Q')
-                return false;
+    bool is_valid(vector<string> &board,int row,int col){
+        for(int i=row;i>=0;--i)
+            if(board[i][col]=='Q') return false;
+        for(int i=row,j=col;i>=0 && j>=0;--i,--j){
+            if(board[i][j]=='Q') return false;
         }
-            for( int i=row,j=col; i>=0 and j>=0; i--,j--)
-            {
-                if(board[i][j]=='Q')
-                {
-                    return false;
-                }
-            }
-        
-        for(int  i=row,j=col; i>=0 and j<board.size(); i--,j++)
-        {
-            if(board[i][j]=='Q')
-            {
-                return false;
-            }
-        }
-        
-        
+        for(int i=row,j=col;i>=0 && j<board.size();--i,++j)
+            if(board[i][j]=='Q') return false;
         return true;
-            
-        
     }
-    void dfs(vector<string>& board, int row)
-    {
-        if(row==board.size())
-        {
+    
+    void dfs(vector<string> &board,int row){
+        if(row==board.size()){
             res.push_back(board);
             return;
         }
-        for(int i=0; i<board.size(); i++)
-        {
-            if(is_Valid(board,row,i))
-            {
+        for(int i=0;i<board.size();i++){
+            if(is_valid(board,row,i)){
                 board[row][i]='Q';
                 dfs(board,row+1);
                 board[row][i]='.';
@@ -54,4 +30,5 @@ public:
         dfs(board,0);
         return res;
     }
+    
 };
