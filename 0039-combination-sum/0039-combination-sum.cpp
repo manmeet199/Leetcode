f951d538122c56e2e062e9e7c954f2e877d6a206
Manmeet Singh Chhabra <manmeett29@gmail.com>
2022-11-01 19:09:05 +0530

    Time: 20 ms (39.53%), Space: 10.8 MB (70.91%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void solve(vector<int>& nums, int i, int cs,int target, vector<int>& temp, vector<vector<int>>& res )
+    {
+        if(cs>target)
+            return;
+        if(cs==target)
+        {
+         res.push_back(temp);
+            return;
+        }
+        for(int j=i; j<nums.size(); j++)
+        {
+            temp.push_back(nums[j]);
+            cs+=nums[j];
+            solve(nums,j,cs,target,temp,res);// rec within same subtree
+            cs-=nums[j];
+            
+            temp.pop_back();
+            
+            
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        solve(nums,0,0,target,temp,res);
+        return res;
+    }
+};
